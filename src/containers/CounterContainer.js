78be24a402e74b697ef3923227f0dba9ff6e4e89
Vladimir Kozlovsky <vovakozlouskiy@gmail.com>
2021-10-14 17:50:41 +0300

    Lifecycle methods added

--- src/containers/CounterContainer.js
+++ src/containers/CounterContainer.js
@@ -12,6 +12,41 @@ export default class CounterContainer extends React.Component {
         this.onResetClicked = this.onResetClicked.bind(this);
     }
 
+    componentDidMount() {
+        console.log(`${this.constructor.name} : ${this.componentDidMount.name}`);
+    }
+
+    componentWillReceiveProps(nextProps, nextContext) {
+        console.log(`${this.constructor.name} : ${this.componentWillReceiveProps.name}`);
+        if (nextProps.event === "created") {
+            if (this.state.currentCount % 0 === 0) this.setState(prevState => ({currentCount: prevState + 1}))
+        } else if (nextProps.event === "removed") {
+            if (this.state.currentCount % 0 !== 0) this.setState(prevState => ({currentCount: prevState - 1}))
+        } else {
+            throw new Error("Illegal event name");
+        }
+    }
+
+    shouldComponentUpdate(nextProps, nextState, nextContext) {
+        console.log(`${this.constructor.name} : ${this.shouldComponentUpdate.name}`);
+        return this.state.currentCount !== nextState.currentCount;
+    }
+
+    componentDidUpdate(prevProps, prevState, snapshot) {
+        console.log(`${this.constructor.name} : ${this.componentDidUpdate.name}`);
+    }
+
+    componentWillUnmount() {
+        console.log(`${this.constructor.name} : ${this.componentWillUnmount.name}`);
+    }
+
+    static getDerivedStateFromProps(){
+        console.log(`${this.constructor.name} : ${this.getDerivedStateFromProps.name}`);
+    }
+    getSnapshotBeforeUpdate(prevProps, prevState) {
+        console.log(`${this.constructor.name} : ${this.getSnapshotBeforeUpdate.name}`);
+    }
+
     onIncrementClicked() {
         this.setState(prevState => ({currentCount: prevState.currentCount + 1,}));
     }
@@ -25,6 +60,7 @@ export default class CounterContainer extends React.Component {
     }
 
     render() {
+        console.log(`${this.constructor.name} : ${this.render.name}`);
         return <Counter currentCount={this.state.currentCount} onDecrementClicked={this.onDecrementClicked}
                         onIncrementClicked={this.onIncrementClicked}
                         onResetClicked={this.onResetClicked} {...this.props}/>
