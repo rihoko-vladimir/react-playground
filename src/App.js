9d42eee6c133e28d99c8092996ab0017b10031a3
Vladimir Kozlovsky <vovakozlouskiy@gmail.com>
2021-10-16 22:33:02 +0300

    Task #2 finished

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
 import styles from "./views/Counter/styles.css";
 import CounterContainer from "./containers/CounterContainer";
+import CounterParent from "./views/CounterParent";
 
 function App() {
     return (
         <div style={{"display": "flex", "flex-direction": "row"}}>
             <CounterContainer style={styles}/>
+            <CounterParent style={styles}/>
         </div>
     );
 }
--- src/containers/CounterContainer.js
+++ src/containers/CounterContainer.js
@@ -1,6 +1,7 @@
 import React from "react";
 import Counter from "../views/Counter";
 import PropTypes from "prop-types";
+import "react-dom";
 
 export default class CounterContainer extends React.Component {
     state = {currentCount: 0,};
@@ -12,39 +13,19 @@ export default class CounterContainer extends React.Component {
         this.onResetClicked = this.onResetClicked.bind(this);
     }
 
-    componentDidMount() {
-        console.log(`${this.constructor.name} : ${this.componentDidMount.name}`);
-    }
-
-    componentWillReceiveProps(nextProps, nextContext) {
-        console.log(`${this.constructor.name} : ${this.componentWillReceiveProps.name}`);
+    //this fucking shit does not work. idk what to do to make react call this garbage
+    UNSAFE_componentWillReceiveProps(nextProps, nextContext) {
+        console.log(`${this.constructor.name} : ${this.UNSAFE_componentWillReceiveProps.name}`);
         if (nextProps.event === "created") {
-            if (this.state.currentCount % 0 === 0) this.setState(prevState => ({currentCount: prevState + 1}))
-        } else if (nextProps.event === "removed") {
-            if (this.state.currentCount % 0 !== 0) this.setState(prevState => ({currentCount: prevState - 1}))
-        } else {
-            throw new Error("Illegal event name");
+            if (this.state.currentCount !== 0 && this.state.currentCount % 2 === 0) this.setState(prevState => ({currentCount: prevState.currentCount + 1,}));
+        }
+        if (nextProps.event === "removed") {
+            if (this.state.currentCount % 2 !== 0) this.setState(prevState => ({currentCount: prevState.currentCount - 1,}));
         }
     }
 
     shouldComponentUpdate(nextProps, nextState, nextContext) {
-        console.log(`${this.constructor.name} : ${this.shouldComponentUpdate.name}`);
         return this.state.currentCount !== nextState.currentCount;
-    }
-
-    componentDidUpdate(prevProps, prevState, snapshot) {
-        console.log(`${this.constructor.name} : ${this.componentDidUpdate.name}`);
-    }
-
-    componentWillUnmount() {
-        console.log(`${this.constructor.name} : ${this.componentWillUnmount.name}`);
-    }
-
-    static getDerivedStateFromProps(){
-        console.log(`${this.constructor.name} : ${this.getDerivedStateFromProps.name}`);
-    }
-    getSnapshotBeforeUpdate(prevProps, prevState) {
-        console.log(`${this.constructor.name} : ${this.getSnapshotBeforeUpdate.name}`);
     }
 
     onIncrementClicked() {
@@ -60,7 +41,6 @@ export default class CounterContainer extends React.Component {
     }
 
     render() {
-        console.log(`${this.constructor.name} : ${this.render.name}`);
         return <Counter currentCount={this.state.currentCount} onDecrementClicked={this.onDecrementClicked}
                         onIncrementClicked={this.onIncrementClicked}
                         onResetClicked={this.onResetClicked} {...this.props}/>
