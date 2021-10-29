6198a99e03d92edb9b4120692e81c4c9d4e7fcce
Vladimir Kozlovsky <vovakozlouskiy@gmail.com>
2021-10-29 14:25:13 +0300

    Button order changed for better look

--- src/views/Counter/index.js
+++ src/views/Counter/index.js
@@ -5,9 +5,9 @@ const Counter = (props) => (
     <div style={props.style} className={"mainContainer"}>
         <p className={"counter"}>{props.currentCount}</p>
         <ButtonGroup className={"buttonContainer"} color={"secondary"}>
+            <Button variant="outlined" size={"medium"} onClick={() => props.onIncrementClicked()}>+</Button>
             <Button variant="outlined" size={"medium"} onClick={() => props.onDecrementClicked()}>-</Button>
             <Button variant="outlined" size={"medium"} onClick={() => props.onResetClicked()}>Reset</Button>
-            <Button variant="outlined" size={"medium"} onClick={() => props.onIncrementClicked()}>+</Button>
         </ButtonGroup>
     </div>
 );
